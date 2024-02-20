8b4cab06a32d4ff002c7c85a4163c8cd5f8dabd5
Ani <bloodraven166@gmail.com>
2024-02-20 15:46:50 -0700

    Update plotGenerator.r

--- plotGenerator.r
+++ plotGenerator.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(directlabels)
 
-# Plot generating code
+ggplot(diamonds) + geom_point(aes(x = carat, y = price, color = cut)) + geom_smooth(aes(x = carat, y = price, color = cut)); 
 
 ggsave(filename = 'plot.png', plot = last_plot())
